42c97fcb3bbbea0bbd69ec9d313ccf6686393bdb
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-04 13:36:23 +0100

    Day 04 Part I & II - Kotlin refactoring

--- Kotlin/src/main/kotlin/day04/part1.kt
+++ Kotlin/src/main/kotlin/day04/part1.kt
@@ -7,12 +7,14 @@ val colonSplit = Regex(""":\s+""")
 val pipeSplit = Regex("""\s+\|\s+""")
 val wsSplit = Regex("""\s+""")
 
-fun intersection(line: String) = line.split(pipeSplit).map {
-        it.split(wsSplit).map(String::toInt) }.let { (a, b) -> a.toSet() intersect b.toSet() }
+fun intersectionSize(line: String) = line.split(pipeSplit).map {
+        it.split(wsSplit).map(String::toInt) }.let {
+            (a, b) -> a.toSet() intersect b.toSet() }.run { size }
+
+fun Sequence<String>.winning() = this.map { it.split(colonSplit)[1] }.map(::intersectionSize)
 
 fun main() {
     File("src/main/resources/day_4_input.txt").useLines { file ->
-        file.map { it.split(colonSplit)[1] }.map(::intersection).map(Set<Int>::size)
-            .filter { it > 0 }.map { 2.0.pow(it - 1) }.sum().let(::println)
+        file.winning().filter { it > 0 }.map { 2.0.pow(it - 1) }.sum().let(::println)
     }
 }
--- Kotlin/src/main/kotlin/day04/part2.kt
+++ Kotlin/src/main/kotlin/day04/part2.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 fun main() {
     File("src/main/resources/day_4_input.txt").useLines { file ->
-        val cards = file.map { it.split(colonSplit)[1] }.map(::intersection).map(Set<Int>::size).toList()
+        val cards = file.winning().toList()
         val multiple = IntArray(cards.size) { 1 }
         cards.forEachIndexed { i, c -> with(multiple[i]) {
             (i+1..i+c).forEach { multiple[it] += this }
