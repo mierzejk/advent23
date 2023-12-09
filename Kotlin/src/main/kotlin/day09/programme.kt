a5eeb6620efc364b477a085eb1c2d1f07c421698
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-09 17:20:15 +0100

    Day 09 - Kotlin refactoring.

--- Kotlin/src/main/kotlin/day09/programme.kt
+++ Kotlin/src/main/kotlin/day09/programme.kt
@@ -2,24 +2,21 @@ package day09
 
 import java.io.File
 
-fun getPrediction(collection: List<Long>, backwards: Boolean = false): Long = with (collection) {
+fun getPrediction(collection: List<Long>): Long = with (collection) {
     if (1 == toSet().size)
         get(0)
     else
-        getPrediction(collection.zipWithNext().map { (a, b) -> b - a }, backwards).let { when (backwards) {
-            true -> get(0) - it
-            false -> last() + it
-        } }
-}
+        last() + getPrediction(collection.zipWithNext().map { (a, b) -> b - a })
+    }
 
 fun main() {
-    fun backwards(collection: List<Long>) = getPrediction(collection, true)
-    val total: Sequence<String>.((List<Long>) -> Long) -> Long = {
-        func -> this.map { it.split(' ').map(String::toLong).let(func) }.sum() }
+    fun Sequence<String>.total(backwards: Boolean = false) = map { with ( when(backwards)
+        { false -> it.split(' '); true -> it.split(' ').asReversed() })
+        { map(String::toLong).let(::getPrediction) } }.sum()
 
     // Part I
-    File("src/main/resources/day_9_input.txt").useLines { it.total(::getPrediction) }.also(::println)
+    File("src/main/resources/day_9_input.txt").useLines { it.total() }.also(::println)
 
     // Part II
-    File("src/main/resources/day_9_input.txt").useLines { it.total(::backwards) }.also(::println)
+    File("src/main/resources/day_9_input.txt").useLines { it.total(backwards = true) }.also(::println)
 }
