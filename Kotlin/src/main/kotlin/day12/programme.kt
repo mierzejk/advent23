9947e6fd12a36f063f33f2713efe13ea5a894ea2
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-12 15:41:39 +0100

    Day 12 - Kotlin Part I refactoring completed.

--- Kotlin/src/main/kotlin/day12/programme.kt
+++ Kotlin/src/main/kotlin/day12/programme.kt
@@ -1,8 +1,7 @@
 package day12
 
 import java.io.File
-import kotlin.math.max
-import kotlin.math.min
+import kotlin.math.*
 
 internal class DefaultMap<K, V>(private val defaultValue: (key: K) -> V): HashMap<K, V>() {
     override fun get(key: K) = super.get(key) ?: defaultValue(key).also { this[key] = it }
@@ -12,33 +11,22 @@ internal class DefaultMap<K, V>(private val defaultValue: (key: K) -> V): HashMa
 internal val regexMap = DefaultMap<Int, Regex> { Regex("""[#?]{$it}(\.|\?|$)""") }
 
 internal fun String.matches(at: Int, erasureCode: Int) =
-    regexMap.getValue(erasureCode).matches(slice(at..min(at + erasureCode, lastIndex)))
+    regexMap[erasureCode].matches(slice(at..min(at + erasureCode, lastIndex)))
 
-internal fun String.backtrack(at: Int, erasureCodes: List<Int>): Int {
-    if (erasureCodes.isEmpty())
-        return ('#' !in substring(min(at, length))).compareTo(false)
-
-//    var total = 0
-    return erasureCodes[0].let { code ->
-        (at..length - code)
-            .filter { '#' !in substring(at, max(at, it)) && '.' != this[it] && matches(it, code) }
-            .sumOf { backtrack(it + code + 1, erasureCodes.drop(1)) }
-//        ) {
-//            total += backtrack(i + code + 1, erasureCodes.drop(1))
-//        }
-//        for (i in (at..length - code)
-//            .filter { '#' !in substring(at, max(at, it)) && '.' != this[it] && matches(it, code)}) {
-//            total += backtrack(i + code + 1, erasureCodes.drop(1))
-//        }
+internal fun String.backtrack(at: Int, codesLeft: List<Int>): Int = when {
+    // Accept the branch (+1) only if there is no '#' remaining in the line remainder ahead; otherwise return 0.
+    codesLeft.isEmpty() -> ('#' !in substring(min(at, length))).compareTo(false)
+    else -> codesLeft[0].let { code -> (at..length - code)
+        // Follow a branch only if this is an acceptable starting point and there is no '#' left from the last covered span.
+        .filter { '#' !in substring(at, max(at, it)) && '.' != this[it] && matches(it, code) }
+        .sumOf { backtrack(it + code + 1, codesLeft.drop(1)) }
     }
-
-//    return total
 }
 
 fun main() {
-    val op = File("src/main/resources/day_12_input.txt").useLines { file ->
+    // Part I
+    File("src/main/resources/day_12_input.txt").useLines { file ->
         file.map { it.split(' ').let { (line, codes) ->
-            line.backtrack(0, codes.split(',').map(String::toInt)) }}.toList() }
-    println(op.sum())
-    // 10185 your answer is too high
+            line.backtrack(0, codes.split(',').map(String::toInt)) }}.sum() }
+        .also(::println)
 }
