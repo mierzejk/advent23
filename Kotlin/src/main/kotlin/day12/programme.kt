1c0238d40e976a2cdc1f0599493274c5ee42ae07
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-12 14:56:13 +0100

    Day 12 - Kotlin Part I complete.

--- Kotlin/src/main/kotlin/day12/programme.kt
+++ Kotlin/src/main/kotlin/day12/programme.kt
@@ -13,22 +13,20 @@ internal val regexMap = DefaultMap<Int, Regex> { Regex("""[#?]{$it}(\.|\?|$)""")
 internal fun String.matches(at: Int, erasureCode: Int) =
     regexMap.getValue(erasureCode).matches(slice(at..min(at + erasureCode, lastIndex)))
 
-internal fun String.backtrack(at: Int, codeIndex: Int, erasureCodes: List<Int>): Int {
-    if (codeIndex == erasureCodes.size)
-//        return 1
+internal fun String.backtrack(at: Int, erasureCodes: List<Int>): Int {
+    if (erasureCodes.isEmpty())
         return if (at < length && '#' in substring(at)) 0 else 1
 
     var total = 0
-    erasureCodes[codeIndex].let { code ->
+    erasureCodes[0].let { code ->
         for (i in at..length - code) {
-            val groups = Regex("""#+""").findAll(substring(0, i)).count()
-            if (codeIndex < groups)
+            if (at < i && '#' in substring(at, i - 1))
                 break
 
             if ('.' == this[i] || 0 < i && '#' == this[i-1] || !matches(i, code)) // Is correct starting point?
                 continue
 
-            total += backtrack(i + code + 1, codeIndex + 1, erasureCodes)
+            total += backtrack(i + code + 1, erasureCodes.drop(1))
         }
     }
 
@@ -36,9 +34,9 @@ internal fun String.backtrack(at: Int, codeIndex: Int, erasureCodes: List<Int>):
 }
 
 fun main() {
-    val op = File("src/main/resources/test.txt").useLines { file ->
-        file.take(10).map { it.split(' ').let { (line, codes) ->
-            line.backtrack(0, 0, codes.split(',').map(String::toInt)) }}.toList() }
-    println(op)
+    val op = File("src/main/resources/day_12_input.txt").useLines { file ->
+        file.map { it.split(' ').let { (line, codes) ->
+            line.backtrack(0, codes.split(',').map(String::toInt)) }}.toList() }
+    println(op.sum())
     // 10185 your answer is too high
 }
