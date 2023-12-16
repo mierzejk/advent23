38e33261f7929ff48b66dfbd34a03cf5b618163b
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-16 12:51:46 +0100

    Day 15 - Kotlin Part II complete.

--- Kotlin/src/main/kotlin/day15/programme.kt
+++ Kotlin/src/main/kotlin/day15/programme.kt
@@ -2,11 +2,26 @@ package day15
 
 import java.io.File
 
-private fun hash(acc: Long, c: Char) = (((acc + c.code.toLong()) * 17L) % 256L)
-
-private fun hash(string: CharSequence): Long = string.fold(0L, ::hash)
+private fun hash(string: CharSequence): Int = string.fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
 
 fun main() {
-    File("src/main/resources/day_15_input.txt").readText().filterNot { '\n' == it  }
-        .split(',').sumOf(::hash).also(::println)
+    val input = File("src/main/resources/day_15_input.txt").readText().filterNot { '\n' == it  }
+        .split(',')
+
+    // Part I
+    input.sumOf(::hash).also(::println)
+
+    // Part II
+    val boxes = List(256) { LinkedHashMap<String, Int>(5) }
+    input.forEach {
+        val label = it.takeWhile(Char::isLetter)
+        val box = boxes[hash(label)]
+        when (it[label.length]) {
+            '-' -> box.remove(label)
+            else -> box[label] = it.takeLast(1).toInt()
+        }
+    }
+
+    boxes.mapIndexed { i, box -> (i + 1) * box.values.foldIndexed(0) { j, acc, fl -> fl * (j + 1) + acc } }
+        .sum().also(::println)
 }
