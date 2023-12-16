69c446928720fc07e58b3e550a4b3d2abb139a54
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-16 15:01:20 +0100

    Day 16 - Kotlin Part II progress.

--- Kotlin/src/main/kotlin/day16/programme.kt
+++ Kotlin/src/main/kotlin/day16/programme.kt
@@ -1,16 +1,16 @@
 package day16
 
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 fun main() {
     val input = File("src/main/resources/day_16_input.txt").readLines()
-    val contraption =  input.joinToString("").toCharArray()
+    val contraption = input.joinToString("").toCharArray()
     val stride = input[0].length
     val direction = object { val Up = -stride; val Right = 1; val Down = stride; val Left = -1 }
 
     data class Beam(val pos: Int, val dir: Int) {
         private fun Int.move() = Beam(pos + this, this)
-
 
         @Suppress("SpellCheckingInspection")
         fun geradeaus() = listOf(dir.move())
@@ -22,56 +22,75 @@ fun main() {
         fun horizontal() = listOf(direction.Left.move(), direction.Right.move())
     }
 
-    var queue: ArrayDeque<Beam>// = ArrayDeque<Beam>(12).apply { add(Beam(0, direction.Right)) }
-    var visited: List<MutableSet<Int>> = emptyList()// = List(contraption.size) { mutableSetOf<Int>() }.apply { this[0] += queue[0].dir }
+    var queue: ArrayDeque<Beam>
+    var visited: List<MutableSet<Int>>
 
-    fun trace(beam: Beam) {
+    fun trace(beam: Beam): Int {
         queue = ArrayDeque<Beam>(12).apply { add(beam) }
         visited = List(contraption.size) { mutableSetOf<Int>() }.apply { this[0] += queue[0].dir }
 
         while (queue.isNotEmpty()) {
-            queue.removeFirst().run { when (contraption[pos]) {
-                '.' -> geradeaus()
-                '|' -> when (dir) {
-                    direction.Up, direction.Down -> geradeaus()
-                    direction.Left, direction.Right -> vertical()
-                    else -> throw IllegalArgumentException("$dir")
+            queue.removeFirst().run {
+                when (contraption[pos]) {
+                    '.' -> geradeaus()
+                    '|' -> when (dir) {
+                        direction.Up, direction.Down -> geradeaus()
+                        direction.Left, direction.Right -> vertical()
+                        else -> throw IllegalArgumentException("$dir")
+                    }
+
+                    '-' -> when (dir) {
+                        direction.Left, direction.Right -> geradeaus()
+                        direction.Up, direction.Down -> horizontal()
+                        else -> throw IllegalArgumentException("$dir")
+                    }
+
+                    '/' -> when (dir) {
+                        direction.Up -> right()
+                        direction.Right -> up()
+                        direction.Down -> left()
+                        direction.Left -> down()
+                        else -> throw IllegalArgumentException("$dir")
+                    }
+
+                    '\\' -> when (dir) {
+                        direction.Up -> left()
+                        direction.Right -> down()
+                        direction.Down -> right()
+                        direction.Left -> up()
+                        else -> throw IllegalArgumentException("$dir")
+                    }
+
+                    else -> throw IllegalArgumentException("(${pos / stride},${pos % stride}): ${contraption[pos]}")
                 }
-                '-' -> when (dir) {
-                    direction.Left, direction.Right -> geradeaus()
-                    direction.Up, direction.Down -> horizontal()
-                    else -> throw IllegalArgumentException("$dir")
+            }.filter {
+                when {
+                    it.pos !in contraption.indices -> false
+                    direction.Left == it.dir && 0 == (it.pos + 1) % stride -> false
+                    direction.Right == it.dir && 0 == (it.pos) % stride -> false
+                    it.dir in visited[it.pos] -> false
+                    else -> true
                 }
-                '/' -> when (dir) {
-                    direction.Up -> right()
-                    direction.Right -> up()
-                    direction.Down -> left()
-                    direction.Left -> down()
-                    else -> throw IllegalArgumentException("$dir")
-                }
-                '\\' -> when (dir) {
-                    direction.Up -> left()
-                    direction.Right -> down()
-                    direction.Down -> right()
-                    direction.Left -> up()
-                    else -> throw IllegalArgumentException("$dir")
-                }
-                else -> throw IllegalArgumentException("(${pos / stride},${pos % stride}): ${contraption[pos]}")
-            } }.filter { when {
-                it.pos !in contraption.indices -> false
-                direction.Left == it.dir && 0 == (it.pos + 1) % stride -> false
-                direction.Right == it.dir && 0 == (it.pos) % stride -> false
-                it.dir in visited[it.pos] -> false
-                else -> true
-            } }.forEach {
+            }.forEach {
                 visited[it.pos] += it.dir
                 queue.add(it)
             }
         }
+
+        return visited.count { it.isNotEmpty() }
     }
 
-    trace(Beam(0, direction.Right))
-    visited.count { it.isNotEmpty() }.also(::println)
+    measureTimeMillis {
+        trace(Beam(0, direction.Right)).also(::println)
+    }.also { println("timeit: $it") }
 
     // Part II
+    measureTimeMillis {
+        listOf(
+            input.indices.map { Beam(it * stride, direction.Right) }
+        ).flatMap { it.map(::trace) }.max().also { println("Max: $it") }
+//        input.indices.maxOfOrNull {
+//            trace(Beam(it * stride, direction.Right))
+//        }.also { println("Max: $it") }
+    }.also { println("timeit: $it") }
 }
