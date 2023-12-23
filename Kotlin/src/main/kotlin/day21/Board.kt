524ab1e146db2e8825bc5ab345af6ade6e9a557c
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-23 21:26:28 +0100

    Day 21 - Part I progress.

---
+++ Kotlin/src/main/kotlin/day21/Board.kt
@@ -0,0 +1,30 @@
+package day21
+
+internal open class Board<T>(val array: List<T>, val stride: Int, height: Int? = null) {
+    private val lastButStride = array.lastIndex - stride
+    val height = height ?: (array.size / stride)
+
+    init { assert (this.height * stride == array.size) }
+
+    fun spaceLeft(index: Int) = 0 != index % stride
+    fun indexLeft(index: Int) = if (spaceLeft(index)) index - 1 else null
+    fun distanceLeft(index: Int) = index % stride
+    fun elementLeft(index: Int, default: T? = null) = indexLeft(index)?.let(array::get) ?: default
+    fun spaceUp(index: Int) = stride <= index
+    fun indexUp(index: Int) = if (spaceUp(index)) index - stride else null
+    fun distanceUp(index: Int) = index / stride
+    fun elementUp(index: Int, default: T? = null) = indexUp(index)?.let(array::get) ?: default
+    fun spaceRight(index: Int) = 0 != (index + 1) % stride
+    fun indexRight(index: Int) = if (spaceRight(index)) index + 1 else null
+    fun distanceRight(index: Int) = stride - 1 - distanceLeft(index)
+    fun elementRight(index: Int, default: T? = null) = indexRight(index)?.let(array::get) ?: default
+    fun spaceDown(index: Int) = index <= lastButStride
+    fun indexDown(index: Int) = if (spaceDown(index)) index + stride else null
+    fun distanceDown(index: Int) = height - 1 - distanceUp(index)
+    fun elementDown(index: Int, default: T? = null) = indexDown(index)?.let(array::get) ?: default
+
+    override fun toString() = array.map(Any?::toString).chunked(stride).joinToString("\n")
+}
+
+internal open class MutableBoard<T>(protected val mutableArray: MutableList<T>, stride: Int, height: Int? = null)
+    : Board<T>(mutableArray, stride, height)
--- Kotlin/src/main/kotlin/day21/programme.kt
+++ Kotlin/src/main/kotlin/day21/programme.kt
@@ -2,28 +2,6 @@ package day21
 
 import java.io.File
 
-internal open class Board<T>(val array: List<T>, val stride: Int, height: Int? = null) {
-    private val lastButStride = array.lastIndex - stride
-    val height = height ?: (array.size / stride)
-
-    init { assert (this.height * stride == array.size) }
-
-    fun Int.spaceLeft() = 0 != this % stride
-    fun Int.distanceLeft() = this % stride
-    fun Int.spaceUp() = stride <= this
-    fun Int.distanceUp() = this / stride
-    fun Int.spaceRight() = 0 != (this + 1) % stride
-    fun Int.distanceRight() = stride - 1 - this.distanceLeft()
-    fun Int.spaceDown() = this <= lastButStride
-    fun Int.distanceDown() = height - 1 - this.distanceUp()
-
-    override fun toString() = array.map(Any?::toString).chunked(stride).joinToString("\n")
-
-}
-
-internal class MutableBoard<T>(private val mutableArray: MutableList<T>, stride: Int, height: Int? = null)
-    : Board<T>(mutableArray, stride, height)
-
 fun main() {
     val array = mutableListOf<Short>()
     var stride = 0
