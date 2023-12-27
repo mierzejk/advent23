df7896204a92123cba53fa809a259c0a99e41a2f
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-27 11:16:40 +0100

    Day 23 - Part II complete.

--- Kotlin/src/main/kotlin/day23/programme.kt
+++ Kotlin/src/main/kotlin/day23/programme.kt
@@ -5,13 +5,13 @@ import java.io.File
 internal data class Node(val index: Int) {
     val outcoming = mutableSetOf<Arc>()
 
-    fun addOutcoming(to: Node, length: Int) = outcoming.add(Arc(to, length))
+    fun addOutcoming(to: Node, length: Int) = Arc(to, length).also(outcoming::add)
 
     inner class Arc(val to: Node, val length: Int)
 }
 
 fun main() {
-    val maze = File("src/main/resources/test.txt").useLines { file ->
+    var maze = File("src/main/resources/day_23_input.txt").useLines { file ->
         val iterator = file.iterator()
         val sb = StringBuilder(iterator.next())
         var height = 1
@@ -19,9 +19,10 @@ fun main() {
         iterator.forEachRemaining { sb.append(it); height++ }
         Maze(sb, stride, height)
     }
-    val root = Node(1)
+    var root = Node(1)
     val sink = Node(maze.size - 2)
     val nodes = mutableMapOf(root.index to root, sink.index to sink)
+    val arcs = ArrayList<Node.Arc>()
 
     class Path(val from: Node, private var next: Int?): Iterable<Int>, Iterator<Int> {
         var length = 0
@@ -55,16 +56,20 @@ fun main() {
         return adjacent.map { Path(node, it) }
     }
 
-    // Breadth-First Search
-    val queue = ArrayDeque<Path>().apply { add(Path(root, maze.indexDown(root.index))) }
-    while (queue.isNotEmpty()) {
-        val path = queue.removeFirst()
-        val end = path.last()
-        val outcoming = addNode(end)
-        nodes[end]?.let { path.from.addOutcoming(it, path.length) }
-        if (outcoming.isNotEmpty())
-            queue.addAll(outcoming)
+    fun bfs(starting: Path) {
+        val queue = ArrayDeque<Path>().apply { add(starting) }
+        while (queue.isNotEmpty()) {
+            val path = queue.removeFirst()
+            val end = path.last()
+            val outcoming = addNode(end)
+            nodes[end]?.let { path.from.addOutcoming(it, path.length).also(arcs::add) }
+            if (outcoming.isNotEmpty())
+                queue.addAll(outcoming)
+        }
     }
+
+    // Breadth-First Search to create the graph.
+    bfs(Path(root, maze.indexDown(root.index)))
 
     // assert(maze is DAG)
     // Topological sort - Depth-First Search
@@ -93,6 +98,38 @@ fun main() {
     }
 
     // Part I
-    print(distance[sink.index])
+    println(distance[sink.index])
+
+    // Part II
+    class Route(val head: Node, val tail: Set<Int>, val length: Int) {
+        // Assumption - there is no loops.
+        fun expand(): List<Route> {
+            return head.outcoming.filterNot { it.to.index in tail }.map {
+                Route(it.to, tail + head.index, it.length + length) }
+        }
+    }
+
+    nodes.clear()
+    arcs.clear()
+    maze = Maze(maze.array.map { when(it) {
+        '#' -> '#'
+        else -> '.'
+    } }, maze.stride, maze.height)
+    root = Node(1)
+    nodes[root.index] = root
+    nodes[sink.index] = sink
+
+    bfs(Path(root, maze.indexDown(root.index)))
+    val completeRoutes = mutableListOf<Route>()
+    val routeQueue = ArrayDeque<Route>()
+    routeQueue.add(Route(root, emptySet(), 0))
+    while(routeQueue.isNotEmpty()) {
+        val routes = routeQueue.removeFirst().expand()
+        val (finished, pending) = routes.partition { it.head === sink }
+        completeRoutes.addAll(finished)
+        routeQueue.addAll(pending)
+    }
+
+    print(completeRoutes.maxOf(Route::length))
 }
 
