ef1bb787f57ee8fd4a63c35bda9531f3c76e2b29
Krzysztof Mierzejewski <kmierz@gmail.com>
2023-12-27 16:29:39 +0100

    Update Maze.kt

--- Kotlin/src/main/kotlin/day23/Maze.kt
+++ Kotlin/src/main/kotlin/day23/Maze.kt
@@ -4,6 +4,7 @@ import day21.Board
 
 class Maze(array: List<Char>, stride: Int, height: Int? = null): Board<Char>(array, stride, height) {
     private val allowed = "<^>v".map{ listOf('.', it) }
+    
     constructor(array: CharSequence, stride: Int, height: Int? = null) : this(array.toList(), stride, height)
 
     override fun getAdjacent(index: Int) =
