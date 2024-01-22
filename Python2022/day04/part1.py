71afafca6598b77ecb25e1674faeeaeeacba4835
Krzysztof Mierzejewski <kmierz@gmail.com>
2024-01-22 16:51:44 +0100

    2022 Day 04.

---
+++ Python2022/day04/part1.py
@@ -0,0 +1,13 @@
+def contains(a: tuple[int, int], b: tuple[int, int]) -> bool:
+    a, b = sorted((a, b))
+    return b[1] <= a[1] or a[0] == b[0]
+
+
+def to_tuple(s: str) -> tuple[int, int]:
+    a, b = s.split(r'-')
+    return int(a), int(b)
+
+
+if r'__main__' == __name__:
+    with open(r'../inputs/day_4_input.txt', encoding=r'utf8') as file:
+        print(sum(contains(to_tuple(a), to_tuple(b)) for a, b in map(lambda line: line.rstrip().split(r','), file)))
---
+++ Python2022/day04/part2.py
@@ -0,0 +1,9 @@
+from day04.part1 import to_tuple
+
+
+def overlap(a: tuple[int, int], b: tuple[int, int]) -> int:
+    return 1 if min(a[1], b[1]) - max(a[0], b[0]) >= 0 else 0
+
+
+with open(r'../inputs/day_4_input.txt', encoding=r'utf8') as file:
+    print(sum(overlap(to_tuple(a), to_tuple(b)) for a, b in map(lambda line: line.rstrip().split(r','), file)))
