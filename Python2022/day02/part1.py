ca1916f311e734b341c5edb66266c95733bc1bb2
Krzysztof Mierzejewski <kmierz@gmail.com>
2024-01-22 13:36:47 +0100

    2022 Day 02.

---
+++ Python2022/day02/part1.py
@@ -0,0 +1,14 @@
+scores = {
+    r'A X': 1 + 3,
+    r'A Y': 2 + 6,
+    r'A Z': 3 + 0,
+    r'B X': 1 + 0,
+    r'B Y': 2 + 3,
+    r'B Z': 3 + 6,
+    r'C X': 1 + 6,
+    r'C Y': 2 + 0,
+    r'C Z': 3 + 3
+}
+
+with open(r'../inputs/day_2_input.txt', encoding=r'utf8') as file:
+    print(sum(scores[line.rstrip()] for line in file))
---
+++ Python2022/day02/part2.py
@@ -0,0 +1,14 @@
+scores = {
+    r'A X': 3 + 0,  # lose
+    r'A Y': 1 + 3,  # draw
+    r'A Z': 2 + 6,  # win
+    r'B X': 1 + 0,  # lose
+    r'B Y': 2 + 3,  # draw
+    r'B Z': 3 + 6,  # win
+    r'C X': 2 + 0,  # lose
+    r'C Y': 3 + 3,  # draw
+    r'C Z': 1 + 6   # win
+}
+
+with open(r'../inputs/day_2_input.txt', encoding=r'utf8') as file:
+    print(sum(scores[line.rstrip()] for line in file))
